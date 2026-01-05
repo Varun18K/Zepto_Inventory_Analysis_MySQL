56d7fd034f26129b450ddda199ba8ae621b0f98c
Varun18K <karlevarun18@gmail.com>
2026-01-05 09:09:29 +0530

    Create 01_table_creation.sql

---
+++ sql/01_table_creation.sql
@@ -0,0 +1,19 @@
+-- ============================================
+-- STEP 1: Database & Table Creation
+-- ============================================
+
+CREATE DATABASE IF NOT EXISTS zepto_inventory;
+USE zepto_inventory;
+
+CREATE TABLE zepto_inventory (
+    sku_id INT AUTO_INCREMENT PRIMARY KEY,
+    category VARCHAR(100),
+    product_name VARCHAR(255) NOT NULL,
+    mrp DECIMAL(10,2),
+    discount_percent DECIMAL(5,2),
+    available_quantity INT,
+    selling_price DECIMAL(10,2),
+    weight_in_grams INT,
+    out_of_stock BOOLEAN,
+    quantity INT
+);
